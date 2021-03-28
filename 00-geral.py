55997de89019eb51b0e7c68f383a57adcbabf2e9
Josinfo Xtreme <josimaru@gmail.com>
2021-03-28 14:48:04 -0300

    tes

---
+++ 00-geral.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+print ("Hello World!" "\n")
+print("CCIEofPeople" "\n")
+#
+ 
+a1 = "Josinfo"
+a2 = "Xtreme"
+
+print(a1,a2)
+print(a1,a2, sep=":")
+print(a1,a2, sep=" é ")
---
+++ 01-input.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+#Teste 01
+
+nome = input ("Digite seu nome: ")
+print ("Seja bem vindo",nome)
