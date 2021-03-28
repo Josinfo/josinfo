58eb990d170742749bebfd0b4fc8bd17cb15a289
Josinfo Xtreme <josimaru@gmail.com>
2021-03-28 15:57:55 -0300

    tes

---
+++ 02- TIposDados.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+#Inteiros
+a=10
+print (type (a))
+#
+#Flutuante
+b=3.2
+print (type (b))
+#
+#Complexos
+c=5j
+print (type (c))
+#
+
+#Metodos
+var="    Casa para frase abaixo       "
+print("Metoto Title-",var.title()," end")
+print("\n", "Metoto Tittle-",var.upper()," end" "\n")
+print("Metoto Lower-",var.lower()," end")
+print("Metoto replace-",var.replace("a","@")," end")
+print("Metoto strip-",var.strip()," end")
+print("Metoto rstrip-",var.rstrip()," end")
+print("Metoto lstrip-",var.lstrip()," end")
+print("Metoto split-",var.split()," end")
+
+
+
---
+++ 04-Operadores.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+num1 = 5
+num2 = 4
+
+print ("O Numro (",num1,")", "e o Numero (",num2,") São: \n")
+print("Iguais:",num1 == num2 )
+print("Diferentes:",num1 != num2 )
+print("Menor:",num1 < num2 )
+print("Menor ou Igual:",num1 <= num2 )
+print("Maior:",num1 >= num2 )
+print("Maior ou Igual:",num1 >= num2 )
+print("boleano (T or F):",num1 is num2 )
+
+bol1 = True
+bol2 = False
+print("AND (T or F):",bol1 and bol2 )
+print("OR (T or F):",bol1 or bol2 )
