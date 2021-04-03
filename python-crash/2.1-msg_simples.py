62308c80f806aa50c7c7924009cdc357c11fee97
Josinfo Xtreme <josimaru@gmail.com>
2021-04-03 14:45:26 -0300

    TESTE

--- python-crash/2.1-msg_simples.py
+++ python-crash/2.1-msg_simples.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python
 
-msg = ("Python Crash")
-print (msg)
+#Captulo 02 - Variaveis
+msg01 = ("\n Python Crash")
+print (msg01)
+
+#Captulo 02 - String
+msg02 = ("\n Texto")
+print (msg02.title(), msg02.upper(),msg02.lower())
+
+#Captulo 02 - Concatenando
+name = input ("Nome: ")
+surname = input ("Sobrenome: ")
+fullname = (name + " " + surname)
+print ("\n",fullname)
+
+
