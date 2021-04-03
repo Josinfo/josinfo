a70c025486ffbe8d0b5c17febe7d32293ac27418
Josinfo Xtreme <josimaru@gmail.com>
2021-04-03 15:06:31 -0300

    TESTE

---
+++ python-crash/2.2-concatenado.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+#Captulo 02 - Concatenando
+name = input ("Nome: ")
+surname = input ("Sobrenome: ")
+fullname = (name + " " + surname)
+motd = "welcome"
+print ("hello: " + fullname + " "+ motd.upper()+"!")
+
+print("\nLanguages:\nPython\nC\nJava\tScript")
+
+
